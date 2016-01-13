eace0e446d96e7798e8fe758413821620a9e814b
dengweiren <973040432@qq.com>
2016-01-13 15:19:41 +0800

    添加功能

---
+++ 张三啊/张三啊/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  张三啊
+//
+//  Created by 邓伟仁 on 16/1/13.
+//  Copyright © 2016年 邓伟仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 张三啊/张三啊/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  张三啊
+//
+//  Created by 邓伟仁 on 16/1/13.
+//  Copyright © 2016年 邓伟仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
